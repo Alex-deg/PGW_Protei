70f34db3ad8ec1b4c1c52ebfcc8f8c35a307a50d
Alex Degtyarev <degtyarevaleksandr2005@gmail.com>
2025-07-19 19:32:45 +0300

    udp server has been added

---
+++ client/build/CMakeFiles/pgw_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pgw_client.dir/client.cpp.o"
+  "CMakeFiles/pgw_client.dir/client.cpp.o.d"
+  "CMakeFiles/pgw_client.dir/client_main.cpp.o"
+  "CMakeFiles/pgw_client.dir/client_main.cpp.o.d"
+  "pgw_client"
+  "pgw_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pgw_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
